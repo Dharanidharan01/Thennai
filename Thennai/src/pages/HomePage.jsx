465800a0e4a9a91b426df9815aaade8bdf9fecb9
Dharanidharan <dharanidharans175@gmail.com>
2024-11-18 19:10:17 +0530

    Nav bar commit 1

---
+++ Thennai/src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+
+const HomePage = () => {
+  return (
+    <div>
+      <NavBar />
+      <main style={{ padding: '20px' }}>
+        <h1>Welcome to the Home Page</h1>
+        
+      </main>
+    </div>
+  );
+};
+
+export default HomePage;
