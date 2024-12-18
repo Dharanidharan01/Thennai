aaed126e55a5406993f2edcd7eb7f05d95a1f614
Dharanidharan <dharanidharans175@gmail.com>
2024-12-18 12:57:46 +0530

    new commit

---
+++ Thennai/src/ThemeContext.jsx
@@ -0,0 +1,21 @@
+import React, { createContext, useState, useEffect } from 'react';
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState('light');
+
+  useEffect(() => {
+    document.body.setAttribute('data-theme', theme); // Add theme to the body
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
